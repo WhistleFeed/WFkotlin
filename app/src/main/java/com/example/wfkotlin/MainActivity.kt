3af1c016a37721f89400044639d18b9c6cff1148
adithyashetty <aditya.shetty@buddyloan.con>
2022-11-09 17:23:54 +0530

    s2s method implemented

--- app/src/main/java/com/example/wfkotlin/MainActivity.kt
+++ app/src/main/java/com/example/wfkotlin/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.wfkotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.whistlefeed.WhistleFeed
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val whistlefeed: WhistleFeed = findViewById(R.id.webadds)
+        whistlefeed.setadds("1216548416578zJwc4_127", "1")
     }
 }
--- whistle/src/main/java/com/whistle/feedkotlin/WhistleFeed.kt
+++ whistle/src/main/java/com/whistle/feedkotlin/WhistleFeed.kt
@@ -2,11 +2,19 @@ package  com.example.whistlefeed
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.LayoutInflater
 import android.webkit.WebView
+import android.widget.Toast
 import androidx.constraintlayout.widget.ConstraintLayout
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.valueleaf.android_kotlin.WebViewClientImpl
 import com.whistle.feedkotlin.R
+import org.json.JSONException
+import org.json.JSONObject
 
 class WhistleFeed @JvmOverloads constructor(
     context:Context,
@@ -16,31 +24,68 @@ class WhistleFeed @JvmOverloads constructor(
 ) :
    ConstraintLayout(context,attrs,defStyleAttr) {
        private var webView: WebView?=null
+    var mRequestQueue: RequestQueue? = null
+    var mStringRequest: StringRequest? = null
+    var BASE_URL = ""
+    var STAGING_URL = "http://13.232.216.75/whistle-pixel/feedAds.php?"
+    var LIVE_URL = "https://pixel.whistle.mobi/feedAds.php?"
+    var islivelink = true
+    var packageName = ""
+    var scripttags = ""
+
     init {
         LayoutInflater.from(getContext()).inflate(R.layout.adds_view, this, true)
         webView = findViewById(R.id.whistleadds)
 
     }
-
     fun setadds(publishertoken : String,pencilsize : String)
     {
-        val packageName = context.packageName
-        System.out.println("Package Name = " + packageName);
+        if(islivelink)
+        {
+            BASE_URL=LIVE_URL
+        }
+        else
+        {
+            BASE_URL=STAGING_URL
+        }
 
-        var Scripttags= """<!DOCTYPE html>
-        <html lang="en">
-       <body>
-<script src="https://pixel.whistle.mobi/feedAds.js" size="$pencilsize" token="$publishertoken" packagename="$packageName"></script>  
-     </body>
-     </html>""";
+        mRequestQueue = Volley.newRequestQueue(context)
 
-        val webSettings = webView!!.settings
-        webSettings.javaScriptEnabled = true
-        val webViewClient = WebViewClientImpl(context)
-        webView!!.webViewClient = webViewClient
-        webView!!.loadData(
-            Scripttags, "text/html", "UTF-8"
-        )
+        // String Request initialized
+
+        // String Request initialized
+        mStringRequest = StringRequest(
+            Request.Method.GET,
+            BASE_URL + "packagename=" + packageName + "&size=" + pencilsize + "&apiToken=" + publishertoken,
+            { response ->
+                Log.i("", "RESPONSE :$response")
+                try {
+                    val jObject = JSONObject(response)
+                    Log.d("jsonresponse", jObject.toString())
+                    scripttags = jObject.getString("data")
+                    Log.d("scripttags", scripttags)
+                    val packageName = context.packageName
+                    System.out.println("Package Name = " + packageName);
+                    if(packageName!="")
+                    {
+                        val webSettings = webView!!.settings
+                        webSettings.javaScriptEnabled = true
+                        val webViewClient = WebViewClientImpl(context)
+                        webView!!.webViewClient = webViewClient
+                        webView!!.loadData(
+                            """$scripttags""", "text/html", "UTF-8"
+                        )
+                    }
+                    System.out.println("Package scripttags = " + scripttags);
+
+                } catch (e: JSONException) {
+                    e.printStackTrace()
+                }
+              //  Toast.makeText(context, "Response :$response", Toast.LENGTH_LONG).show() //display the response on screen
+            }
+        ) { error -> Log.i("", "Error :$error") }
+
+        mRequestQueue!!.add<String>(mStringRequest)
 
     }
    }
